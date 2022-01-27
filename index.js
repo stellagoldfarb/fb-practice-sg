ba3e36e3ed37aa6fb613c562b8010129b9315b46
Marie Stella Goldfarb <stella@Maries-MacBook-Air.local>
2022-01-27 11:14:49 -0500

    added json

---
+++ index.js
@@ -0,0 +1,14 @@
+// import our restuarnts
+const restuarnts = require('./restaurants.json')
+
+// import a set of tools to talk to firebase and Firestore
+
+//connect
+
+console.log(restuarnts[0].name)
+
+// connect to firestore
+
+// create a collection called "restaurants"
+
+// add each restaurant
